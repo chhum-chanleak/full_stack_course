f4659cf5f47ed74c1d29886c158f4eb657fd5f08
Chanleak Chhum <chhum.chanleak1994@gmail.com>
2026-01-13 22:35:14 +0700

    feat: add 1. 2. and 3.

---
+++ git/exercises2.py
@@ -0,0 +1,57 @@
+# 1. Quiz
+# You can obtain a Git repository in one of two ways:
+
+# By taking a local directory that is not under version control and turning it into a Git repository using __________.
+
+# By copying an existing Git repository from elsewhere using __________.
+
+# 2. git init
+# i. Initializing a Repository in an Existing Directory
+
+# a.
+# You have a folder notes/ with files in it.
+# You want Git to start tracking changes there.
+
+# Question:
+# Which command do you run inside notes/?
+
+# b.
+# You ran git init.
+# Are your files automatically tracked?
+
+# c.
+# After initializing, you want to track everything and make the first commit.
+
+# Question:
+# What are the two commands?
+
+# d.
+# You’re not sure whether a directory is already a Git repository.
+
+# Question:
+# How do you confirm?
+
+# 3. git clone
+# i. Why do we use it?
+# ii. Please show syntax of "git clone"
+
+# a.
+# You want to download a GitHub repository to your machine.
+
+# Question:
+# Which Git command do you use?
+
+# b.
+# You want the repository cloned into a folder called backend instead of the default name.
+
+# Question:
+# What does the command look like?
+
+# c.
+# After cloning, do you need to run git init?
+
+# d.
+# You cloned a repository and want to confirm it’s a Git repository.
+
+# Question:
+# Name one way to check.
