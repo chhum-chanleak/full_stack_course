75824d8e7066fa7e999f0aa61cfe5444ca3f32a3
Chanleak Chhum <chhum.chanleak1994@gmail.com>
2026-01-16 20:46:57 +0700

    feat: add 4. and 5.

--- git/exercises1.py
+++ git/exercises1.py
@@ -28,3 +28,54 @@
 # How do you configure this, and how do you use it?
 
 # 3. Git config scopes — what is the priority order?
+
+# 4. git init
+# i. Initializing a Repository in an Existing Directory
+
+# a.
+# You have a folder notes/ with files in it.
+# You want Git to start tracking changes there.
+
+# Question:
+# Which command do you run inside notes/?
+
+# b.
+# You ran git init.
+# Are your files automatically tracked?
+
+# c.
+# After initializing, you want to track everything and make the first commit.
+
+# Question:
+# What are the two commands?
+
+# d.
+# You’re not sure whether a directory is already a Git repository.
+
+# Question:
+# How do you confirm?
+
+# 5. git clone
+# i. Why do we use it?
+# ii. Please show syntax of "git clone"
+
+# a.
+# You want to download a GitHub repository to your machine.
+
+# Question:
+# Which Git command do you use?
+
+# b.
+# You want the repository cloned into a folder called backend instead of the default name.
+
+# Question:
+# What does the command look like?
+
+# c.
+# After cloning, do you need to run git init?
+
+# d.
+# You cloned a repository and want to confirm it’s a Git repository.
+
+# Question:
+# Name one way to check.
