20d7d4b44b0f6079e3b721d764496e7c028efd68
Chanleak Chhum <chhum.chanleak1994@gmail.com>
2026-02-04 18:33:35 +0700

    docs: add 1. git push

---
+++ git/exercises3.py
@@ -0,0 +1,26 @@
+# 1. git push
+# i. Why do we use "git push"?
+
+# a. Basic push
+
+# Scenario: You created lesson_03.py, added it, and committed locally. Your branch is main.
+
+# Question: What command pushes your changes to the remote repository?
+
+# b. Pushing a new branch
+
+# Scenario: You created a branch feature/loops to add exercises. You committed loops_exercises.py.
+
+# Question: What command pushes this branch and sets it to track the remote?
+
+# c. Force push
+
+# Scenario: You amended a commit on main (changed the message) and need to update the remote.
+
+# Question: What command will safely overwrite the remote branch?
+
+# d. Pushing a tag
+
+# Scenario: You tagged a release v1.0 after finishing the exercises.
+
+# Question: How do you push this tag to the remote repository?
